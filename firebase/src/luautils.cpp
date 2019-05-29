2ab4cc93d53ef4297d1a2d6705a19577de749763
Alexey Gulev <Alexey.Gulev@king.com>
2019-05-29 17:27:33 +0200

    Removed dekstop platforms

--- firebase/src/luautils.cpp
+++ firebase/src/luautils.cpp
@@ -4,18 +4,18 @@
 void luaL_checklistener(lua_State* L, int idx, struct lua_Listener& listener) {
 	int top = lua_gettop(L);
 	luaL_checktype(L, idx, LUA_TFUNCTION);
-    lua_pushvalue(L, idx);
-    int cb = dmScript::Ref(L, LUA_REGISTRYINDEX);
+	lua_pushvalue(L, idx);
+	int cb = dmScript::Ref(L, LUA_REGISTRYINDEX);
 
-    if (listener.m_Callback != LUA_NOREF) {
-      dmScript::Unref(listener.m_L, LUA_REGISTRYINDEX, listener.m_Callback);
-      dmScript::Unref(listener.m_L, LUA_REGISTRYINDEX, listener.m_Self);
-    }
+	if (listener.m_Callback != LUA_NOREF) {
+		dmScript::Unref(listener.m_L, LUA_REGISTRYINDEX, listener.m_Callback);
+		dmScript::Unref(listener.m_L, LUA_REGISTRYINDEX, listener.m_Self);
+	}
 
-    listener.m_L = dmScript::GetMainThread(L);
-    listener.m_Callback = cb;
-    dmScript::GetInstance(L);
-    listener.m_Self = dmScript::Ref(L, LUA_REGISTRYINDEX);
+	listener.m_L = dmScript::GetMainThread(L);
+	listener.m_Callback = cb;
+	dmScript::GetInstance(L);
+	listener.m_Self = dmScript::Ref(L, LUA_REGISTRYINDEX);
 	assert(top == lua_gettop(L));
 }
 
