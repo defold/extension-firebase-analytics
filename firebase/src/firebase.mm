d21092466fabab316c50990f2a014532aabe8955
Filazopovich Gennady <g.filazopovich@melsoft-games.com>
2020-02-06 14:36:54 +0300

    Write event to log if it throw exception in LogEvent

--- firebase/src/firebase.mm
+++ firebase/src/firebase.mm
@@ -13,6 +13,27 @@ void Firebase_Safe_LogEvent(lua_State* L, const char* name, const firebase::anal
     }
     @catch(...)
     {
+        dmLogError("Exception occurred for event '%s' with parameters:", name)
+
+        for(size_t index = 0; index < number_of_parameters; ++index)
+        {
+            const auto& parameter = params[index];
+            const auto& value = parameter.value;
+
+            if (value.is_double())
+            {
+                dmLogError("\t'%s' = %f", parameter.name, value.double_value())
+            }
+            else if (value.is_string() || value.is_bool() || value.is_null())
+            {
+                dmLogError("\t'%s' = '%s'", parameter.name, value.AsString().string_value());
+            }
+            else
+            {
+                dmLogError("\t'%s' = unsupported param type", parameter.name)
+            }
+        }
+
         luaL_error(L, "firebase: log event exception. \"%s\"", name);
     }
 }
